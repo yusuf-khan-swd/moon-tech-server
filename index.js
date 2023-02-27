ba08f3420c398b99907d00e7c3f7b925aaeb4283
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 14:27:02 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,6 +7,24 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t99wqyy.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+const run = async () => {
+  try {
+    const db = client.db("moonTech");
+    const productCollection = db.collection("product");
+
+  }
+
+  finally {
+
+  }
+}
+
+run().catch(err => console.log(err));
 
 app.use("/", (req, res) => {
   res.send("MoonTech Server is running");
