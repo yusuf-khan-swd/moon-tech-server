7e126da59fdafb7679a4530df42c62e60532ce90
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-02-27 13:50:12 +0600

    basic server setup done

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/", (req, res) => {
+  res.send("MoonTech Server is running");
+});
+
+app.listen(port, () => {
+  console.log("Server is running on port", port);
+});
